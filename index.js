fca27519ff1748ab97f882f9b2fc43f52bc72493
Claudio W. Germano Jr <>
2021-04-26 18:05:41 +0000

    Operadores lógicos &&, || , !

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ console.log(imc)
 
 const pessoa = {
   nome: "Joana",
-  idade: 64,
+  idade: 72,
   ehMaiordeIdade: true,
   altura: 1.68 
 };
@@ -26,14 +26,62 @@ if (pessoa.idade >= 18) {
   console.log('É maior de idade')
 } else {console.log('É menor de idade')}
 
-if (pessoa.idade <= 18) {console.log('Entrou no if')} else {console.log('Entrou no else')}
 
 // condicionais 2
 
 if (pessoa.idade < 18) {
-  console.log('A pessoa é menor de idade');
+  console.log(pessoa.nome + ' é menor de idade');
   } else if (pessoa.idade < 60) {
-    console.log('A pessoa é adulta');
+    console.log(`${pessoa.nome} é adulto`);
   } else {
-    console.log('A pessoa é idosa')
+    console.log(`${pessoa.nome} é uma pessoa de ${pessoa.idade} anos`)
   }
+
+// concatenar strings e string template:
+
+let texto = pessoa.nome + ' tem ' + pessoa.idade
+console.log(texto)
+
+// operadores lógicos
+
+const crianca = {
+  nome: 'Jose',
+  idade: 19,
+  altura: 1.27,
+  temPassaporte: false
+}
+
+// idade minima = 12 anos
+//altura minima = 1.40
+
+
+if (crianca.idade > 12) {
+  if (crianca.altura > 1.40) {
+    console.log(`${pessoa.nome} pode brincar`);
+  } else {
+    console.log(`${crianca.nome} nao pode brincar porque nao tem a altura minima`)
+  }
+}
+
+//operadores logicos
+
+// && AND
+// || OR
+// ! NOT (PARA BOLEANOS)
+
+if (crianca.idade > 12 || crianca.altura > 1.40) {
+  console.log(`${crianca.nome} pode brincar`)
+} else {
+  console.log(`${crianca.nome} nao pode brincar`)
+}
+
+if (crianca.temPassaporte) {
+  console.log('pode viajar');
+  } else {
+    console.log('nao pode viajar')
+  }
+//USANDO O NOT ! PARA BOLEANOS
+if (!crianca.temPassaporte) {
+  console.log(`${crianca.nome} ESTA BARRADA`)
+}
+
